8f04cec1e9bf52da941f102201db441617de2bc9
moven0831 <moven0831@gmail.com>
2022-09-16 14:24:02 +0800

    remove mint price from smart contract

--- src/MysteryBoxNFT.sol
+++ src/MysteryBoxNFT.sol
@@ -12,24 +12,24 @@ contract NTUFintechClubExpoNFT111_1 is ERC721, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-    bool private revealed = false;
-    uint256 public mintPrice = 3000 wei;
+    bool public revealed = false;
     uint8 public maxSupply = 200;
 
     string private notRevealedURI = "";
     string private baseURI = "";
 
     constructor(string memory _notRevealedURI, string memory _baseURI) ERC721("NTUFintechClubExpoNFT111-1", "NTUFC-EXPO-111") {
-        // https://gateway.pinata.cloud/ipfs/QmR8FizTQTayDHSxHYkFEzaWXhJm2kNL74r5skayZojcYR/
+        // https://gateway.pinata.cloud/ipfs/QmR8FizTQTayDHSxHYkFEzaWXhJm2kNL74r5skayZojcYR
         // https://ipfs.moralis.io:2053/ipfs/Qma2AY1NNAxywYn8CP6Kapibchxhbfz2PnUNHJxq4A7HFS/111-1NTUFC/EXPO/images/mysteryBox/metadata/mysteryBox
+        // https://ipfs.moralis.io:2053/ipfs/QmaHnLTCKuSjkUdhWi4TMYm52t3FnD5WfEkJsbjMJbyYnE
         notRevealedURI = _notRevealedURI;
         // https://gateway.pinata.cloud/ipfs/QmZq9e87AHiWLq9J6fF3cVd7kgFwtvgkHndsXhYnGGxQfx/
         // https://ipfs.moralis.io:2053/ipfs/Qme1cMxpjy8gJtdb65yqwut9hZhTZDSp49Teb3HtrLnQw1/111-1NTUFC/EXPO/metadata/
+        // https://ipfs.moralis.io:2053/ipfs/QmPnDuxFFgHT9suoKmtHf5ygRCwW3cb5satYXSofCNLxBh/
         baseURI = _baseURI;
     }
    
-    function airdropNFT(address to) public payable {
-        require(msg.value >= mintPrice, "Insufficeient balance!");
+    function airdropNFT(address to) external payable onlyOwner {
         require(_tokenIdCounter.current() <= maxSupply, "Exceed max supply of NFT");
 
         uint256 tokenId = _tokenIdCounter.current();
@@ -37,8 +37,8 @@ contract NTUFintechClubExpoNFT111_1 is ERC721, Ownable {
         _safeMint(to, tokenId);
     }
 
-    function switchRevealed(bool _status) external onlyOwner {
-        revealed = _status;   
+    function switchRevealed() external onlyOwner {
+        revealed = !revealed;   
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
